3902c1e5439109a59607e1e8d53b71085575bd33
flael-ux <fs.lael@gmail.com>
2026-03-14 20:14:16 -0300

    Avanço na cena 1

--- Assets/Scripts/Cena1/Cena01Eventos.cs
+++ Assets/Scripts/Cena1/Cena01Eventos.cs
@@ -1,11 +1,29 @@
 using System.Collections;
+using Unity.AppUI.UI;
+using Unity.VectorGraphics.Editor;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class Cena01Eventos : MonoBehaviour
 {
     public GameObject fadeSreenIn;
-    public GameObject charMateo;
-    public GameObject basketball;
+    public GameObject caixaTexto;
+    public GameObject charMateoFala;
+    public GameObject falaMateo;
+    [SerializeField] AudioSource bolaquicando;
+    [SerializeField] string textToSpeak;
+    [SerializeField] int currentTextLength;
+    [SerializeField] int textLength;
+    [SerializeField] GameObject mainTextObject;
+    [SerializeField] GameObject botaoProx;
+    [SerializeField] int eventPos = 0;
+
+
+    void Update()
+    {
+        textLength = CriadordeTexto.charCount;
+    }
+
     void Start()
     {
         StartCoroutine(EventStarter());
@@ -15,12 +33,24 @@ public class Cena01Eventos : MonoBehaviour
     {
         yield return new WaitForSeconds(2);
         fadeSreenIn.SetActive(false);
-        yield return new WaitForSeconds(4);
-        basketball.SetActive(true);
-        //narração o erro da bola de Mateo
         yield return new WaitForSeconds(2);
-        charMateo.SetActive(true);
+        mainTextObject.SetActive(true);
+        textToSpeak = "(Gira a bola de basquete nos dedos, mas ela escapa. Ele solta um riso soprado, sem graça)";
+        caixaTexto.GetComponent<TMPro.TMP_Text>().text = textToSpeak;
+        currentTextLength = textToSpeak.Length;
+        CriadordeTexto.runTextPrint = true;
+        bolaquicando.Play();
+        yield return new WaitForSeconds(0.05f);
+        yield return new WaitForSeconds(1);
+        yield return new WaitUntil(() => textLength == currentTextLength);
+        yield return new WaitForSeconds(0.5f);
+        botaoProx.SetActive(true);
 
+        //caixaTexto.SetActive(true);
+        //yield return new WaitForSeconds(3);
+        //charMateoFala.SetActive(true);
+        //falaMateo.SetActive(true);
     }
+
 
 }
